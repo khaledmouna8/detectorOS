98933d2f43c29aecfef9a2cf3d0c25019f6d590c
MOUNA <khaledmouna8@gmail.com>
2022-05-11 11:53:27 +0100

    v0.0.2

--- detect.js
+++ detect.js
@@ -3,8 +3,8 @@ const button = document.querySelector('.download')
  
 // PC version
 console.log("Browser rezulst: "+ ua.browser.family)
-console.log("Device result: "+ +ua.device.family)
+console.log("Device result: "+ ua.device.family)
 
 // Mobile version 
-alert("Browser result: "+ +ua.browser.family)
-alert("Device result: "+ +ua.device.family)
+alert("Browser result: "+ ua.browser.family)
+alert("Device result: "+ ua.device.family)
