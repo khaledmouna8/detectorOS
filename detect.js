e483d2b000c853b70643ec38855cc39f252d7173
MOUNA <khaledmouna8@gmail.com>
2022-05-11 12:25:49 +0100

    v0.0.5

--- detect.js
+++ detect.js
@@ -2,12 +2,13 @@ const ua = detect.parse(navigator.userAgent)
 const button = document.querySelector('.download')
 
 // Mobile version
-alert("Hello")
-if (ua.device !== null) {
-    //alert("Browser result: "+ ua.browser.family)
-    alert("Full Data: " + JSON.stringify(ua.device))
-    alert("Device result: " + ua.device.family)
-}
+setTimeout(() => {
+    if (ua.device !== null) {
+        //alert("Browser result: "+ ua.browser.family)
+        alert("Full Data: " + JSON.stringify(ua.device))
+        alert("Device result: " + ua.device.family)
+    }
+}, 2000)
 // PC version
 console.log("Full Data: " + JSON.stringify(ua.browser))
 console.log("Browser rezulst: " + ua.browser.family)
